fd29a69d637d17fbb41b095e87da13d4a33b92e8
Naman Saini <namansaini464@gmail.com>
2025-02-11 21:19:10 +0530

    implemented BinaryTree's traversals

--- DSA/3.Trees/binaryTree.cpp
+++ DSA/3.Trees/binaryTree.cpp
@@ -10,6 +10,33 @@ class Node{
 class BinaryTree{  
     private:
     Node* root;
+    void inOrder(Node * root){
+        if (root == NULL)
+        {
+            return;
+        }
+        inOrder(root->left);
+        std::cout << root->data << " ";
+        inOrder(root->right);
+    }
+    void preOrder(Node * root){
+        if (root == NULL)
+        {
+            return;
+        }
+        std::cout << root->data << " ";
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+    void postOrder(Node * root){
+        if (root == NULL)
+        {
+            return;
+        }
+        postOrder(root->left);
+        postOrder(root->right);
+        std::cout << root->data << " ";
+    }
     public:
     BinaryTree(): root(NULL){}
     void insert(int value){
@@ -38,9 +65,52 @@ class BinaryTree{
             q.push(front->right);
             
         }
-        
     }
+    void levelOrderTraversal(){
+        if (root == NULL)
+        {
+            std::cout << "Tree is empty!" << "\n";
+            return;
+        }
+        std::queue<Node*> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            Node* front = q.front();
+            q.pop();
+            std::cout << front->data << " ";
+            if (front-> left != NULL)
+            {
+                q.push(front->left);
+            }
+            if (front->right != NULL)
+            {
+                q.push(front->right);
+            }
+        }
+    }
+    void inOrderTraversal() {
+        inOrder(root);
+        std::cout << "\n";
+    }
+    void preOrderTraversal() {
+        preOrder(root);
+        std::cout << "\n";
+    }
+    void postOrderTraversal() {
+        postOrder(root);
+        std::cout << "\n";
+    }
+
+    
 };
 int main(){
-
+    BinaryTree tree;
+    tree.insert(5);
+    tree.insert(6);
+    tree.insert(7);
+    tree.levelOrderTraversal();
+    tree.inOrderTraversal();
+    tree.preOrderTraversal();
+    tree.postOrderTraversal();
 }
